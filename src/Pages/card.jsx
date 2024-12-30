52c8a5960766d22dd98e54ddbcdc3b2dac3f9c73
Juan Joseph <jsjosep1@ualberta.ca>
2024-12-30 11:00:16 +0530

    update

--- src/Pages/card.jsx
+++ src/Pages/card.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './card.css'; // Import the CSS file for styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
 
 const Card = ({ title, imageUrl, projectLink }) => {
   return (
@@ -7,7 +9,7 @@ const Card = ({ title, imageUrl, projectLink }) => {
     <div className="card">
       <img className="card-image" src={imageUrl} alt={title} />
       <div className="card-content">
-        <h3 className="card-title">{title}</h3>
+        <h3 className="card-title">{title} <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='link-icon'/></h3>
       </div>
     </div>
     </a>
